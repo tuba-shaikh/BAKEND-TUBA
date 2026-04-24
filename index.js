647e14782f2da23246b76ceccf095d33395c59f8
tuba-shaikh <khalid65323shaikh@gmail.com>
2026-04-24 10:50:07 +0530

    test CI/CD pipline

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.get("/",(req, res) => {
     res.send("Hello,Backend is Working!");
 });
 
+app.get("/test",(req,res) =>{
+    res.send("Auto deploy CI/CD");
+});
 app.listen(PORT, () => {
     console.log("Server running on port" + PORT)
 });
